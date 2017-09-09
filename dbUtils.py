eaf886ab191c35a686b654cb4cd0c80e08415915
U-DESKTOP-GIPVFTC\ranga <rangaraj.t@gmail.com>
2017-09-09 12:29:07 -0400

    fixed bugs in file

--- dbUtils.py
+++ dbUtils.py
@@ -1,6 +1,6 @@
 import pymongo
 import googlemaps
-from geopy.distance import vicenity
+from geopy.distance import vincenty
 
 
 client = pymongo.MongoClient("mongodb://admin:admin@firstresponse-shard-00-00-u0yzh.mongodb.net:27017,firstresponse-shard-00-01-u0yzh.mongodb.net:27017,firstresponse-shard-00-02-u0yzh.mongodb.net:27017/test?ssl=true&replicaSet=firstResponse-shard-0&authSource=admin")
@@ -10,30 +10,30 @@ db.victims
 
 def addResponder( responderDictionary ):
     if not responderDictionary.has_key("name"):
-        break
+        return
     if not responderDictionary.has_key("location"):
-        break
+        return
     if not responderDictionary.has_key("phone"):
-        break
+        return
     db.firstResponders.insert_one(responderDictionary)
 
 def addVictim( victimDictionary ):  #adds victim to db and then retuns tuple of the distance to the nearest responder and their name
     if not victimDictionary.has_key("name"):
-        break
+        return
     if not victimDictionary.has_key("location"):
-        break
+        return
     if not victimDictionary.has_key("phone"):
-        break
+        return
     db.victims.insert_one(victimDictionary)
     return findNearestResponder(victimDictionary)
 
-def findNearestResponder( victimDictionary )
+def findNearestResponder( victimDictionary ):
     loc = victimDictionary["location"]
     closestResponder = ()
 
     for item in db.firstResponders.find():
         responderLoc = (item["location"]["lat"], item["locatoin"]["lat"])
-        dist = vicenity(loc, responderLoc).miles
+        dist = vincenty(loc, responderLoc).miles
         if not closestResponder:
             closestResponder = (dist, item)
         elif dist < closestResponder[0]:
@@ -41,5 +41,13 @@ def findNearestResponder( victimDictionary )
 
     return closestResponder
 
+def addResponder( name, location, phone ):
+    lat = location[0]
+    lng = location[1]
 
-#db.firstResponders.insert_one({"name": "test", "location": {"lat": 50, "lng": 50}})
+    db.firstResponders.insert_one({"name": name, "location": {"lat": lat, "lng": lng}, "phone": phone})
+
+name = "Ranga"
+loc = (10, 50)
+phone = 7329978242
+addResponder(name, loc, phone)
