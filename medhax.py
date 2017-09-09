969e0d9ee8fe9ae9fec3cd42ce17764bf3819662
Sail338 <srihari.shankar1@gmail.com>
2017-09-09 01:49:44 -0400

    finished twilio for victim flow, need to insert into db

--- medhax.py
+++ medhax.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,request
 from twilio.rest import Client
-
+from twilio.twiml.messaging_response import MessagingResponse,Message
 app = Flask(__name__)
 
 sid = "AC8632c0885d33bcf38b8eaa6cc6a33f87"
@@ -13,18 +13,34 @@ def index():
 
 @app.route('/sms', methods=['POST'])
 def sms():
+    print (request.form)
     number = request.form['From']
     message_body = request.form['Body']
     if number not in statetabledict:
         statetabledict[(str(number))] = 'help'
+
     if("rescue" in message_body and statetabledict[number] == 'help'):
         __help__(request.form)
+    elif statetabledict[number] == 'waiting for address':
+        print("state is waiting for address")
+        __address__(request.form)
     return "done"
+
 
 def __help__(request_obj):
     number = request_obj['From']
-
-    message = client.messages.create(to=str(number), from_="+18722282071", body="Please Send An Approx address or leave blank for your current location")
+    
+    print("calling help") 
+    message = client.messages.create(to=str(number), from_="+18722282071",
+                                 body="Please send aprox location")
     statetabledict[number] = 'waiting for address'
     return str(message)
 
+def __address__(request_obj):
+    #insert into the database
+    number = request_obj['From']
+    message = client.messages.create(to=str(number), from_="+18722282071",
+                                 body="Stay where you are")
+    return message
+if __name__ == "__main__":
+    app.run(debug=True)
