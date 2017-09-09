9316e0f4c2b6749b871d47c516ee4ee1e842c2e3
Sail338 <srihari.shankar1@gmail.com>
2017-09-08 23:37:50 -0400

    started twilio flows

--- medhax.py
+++ medhax.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from twilio.rest import Client
 
 app = Flask(__name__)
@@ -6,14 +6,25 @@ app = Flask(__name__)
 sid = "AC8632c0885d33bcf38b8eaa6cc6a33f87"
 authtoken = "fba3f82a812fc559b22dd979c7351b9c"
 client = Client(sid, authtoken)
-
+statetabledict = {}
 @app.route("/")
 def index():
     return render_template('home.html')
 
-@app.route('/sms', methods=[POST])
+@app.route('/sms', methods=['POST'])
 def sms():
+    number = request.form['From']
     message_body = request.form['Body']
-    print(message_body)
-message = client.messages.create(to="+12674749730", from_="+18722282071", body="Hello there!")
+    if number not in statetabledict:
+        statetabledict[(str(number))] = 'help'
+    if("rescue" in message_body and statetabledict[number] == 'help'):
+        __help__(request.form)
+    return "done"
+
+def __help__(request_obj):
+    number = request_obj['From']
+
+    message = client.messages.create(to=str(number), from_="+18722282071", body="Please Send An Approx address or leave blank for your current location")
+    statetabledict[number] = 'waiting for address'
+    return str(message)
 
