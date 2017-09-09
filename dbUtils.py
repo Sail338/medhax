eaa6545c8226b0496527142c3b1e87f21ecd110d
U-DESKTOP-GIPVFTC\ranga <rangaraj.t@gmail.com>
2017-09-09 11:13:18 -0400

    Updated dbUtils.py cuz I forgot to save

--- dbUtils.py
+++ dbUtils.py
@@ -1,4 +1,7 @@
 import pymongo
+import googlemaps
+from geopy.distance import vicenity
+
 
 client = pymongo.MongoClient("mongodb://admin:admin@firstresponse-shard-00-00-u0yzh.mongodb.net:27017,firstresponse-shard-00-01-u0yzh.mongodb.net:27017,firstresponse-shard-00-02-u0yzh.mongodb.net:27017/test?ssl=true&replicaSet=firstResponse-shard-0&authSource=admin")
 db = client.firstResponse
@@ -6,17 +9,37 @@ db.firstResponders
 db.victims
 
 def addResponder( responderDictionary ):
-    if not responderDictionary.has_key("Name"):
+    if not responderDictionary.has_key("name"):
         break
-    if not responderDictionary.has_key("Location"):
+    if not responderDictionary.has_key("location"):
+        break
+    if not responderDictionary.has_key("phone"):
         break
     db.firstResponders.insert_one(responderDictionary)
 
-def addVictim( victimDictionary ):
-    if not victimDictionary.has_key("Name"):
+def addVictim( victimDictionary ):  #adds victim to db and then retuns tuple of the distance to the nearest responder and their name
+    if not victimDictionary.has_key("name"):
         break
-    if not victimDictionary.has_key("Location"):
+    if not victimDictionary.has_key("location"):
+        break
+    if not victimDictionary.has_key("phone"):
         break
     db.victims.insert_one(victimDictionary)
+    return findNearestResponder(victimDictionary)
 
-#db.firstResponders.insert_one({"Name": "test", "location": {"latitude": 50, "longitude": 50}})
+def findNearestResponder( victimDictionary )
+    loc = victimDictionary["location"]
+    closestResponder = ()
+
+    for item in db.firstResponders.find():
+        responderLoc = (item["location"]["lat"], item["locatoin"]["lat"])
+        dist = vicenity(loc, responderLoc).miles
+        if not closestResponder:
+            closestResponder = (dist, item)
+        elif dist < closestResponder[0]:
+            closestResponder = (dist, item)
+
+    return closestResponder
+
+
+#db.firstResponders.insert_one({"name": "test", "location": {"lat": 50, "lng": 50}})
