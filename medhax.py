6a3d63506a3393366da0ae481432acdb3d048cc2
Alice Lu <lalice72098@yahoo.com>
2017-09-08 23:05:45 -0400

    add twilio imports

--- medhax.py
+++ medhax.py
@@ -1,7 +1,19 @@
 from flask import Flask,render_template
+from twilio.rest import Client
+
 app = Flask(__name__)
+
+sid = "AC8632c0885d33bcf38b8eaa6cc6a33f87"
+authtoken = "fba3f82a812fc559b22dd979c7351b9c"
+client = Client(sid, authtoken)
 
 @app.route("/")
 def index():
     return render_template('home.html')
+
+@app.route('/sms', methods=[POST])
+def sms():
+    message_body = request.form['Body']
+    print(message_body)
+message = client.messages.create(to="+12674749730", from_="+18722282071", body="Hello there!")
 
--- twilliorescueer.py
+++ twilliorescueer.py
@@ -2,5 +2,4 @@ from twilio.rest import Client
 sid = "AC8632c0885d33bcf38b8eaa6cc6a33f87"
 authtoken = "fba3f82a812fc559b22dd979c7351b9c"
 client = Client(sid, authtoken)
-message = client.messages.create(to="+12674749730", from_="+18722282071",
-                                 body="Hello there!")
+message = client.messages.create(to="+12674749730", from_="+18722282071", body="Hello there!")
