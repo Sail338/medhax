858596c85600ef2aa99ab68f1642bf2f981afdf4
U-DESKTOP-GIPVFTC\ranga <rangaraj.t@gmail.com>
2017-09-10 00:45:38 -0400

    made deleteDB also populate data

--- deleteDBDocs.py
+++ deleteDBDocs.py
@@ -1,4 +1,5 @@
 import pymongo
+import populateDummyData
 
 client = pymongo.MongoClient("mongodb://admin:admin@firstresponse-shard-00-00-u0yzh.mongodb.net:27017,firstresponse-shard-00-01-u0yzh.mongodb.net:27017,firstresponse-shard-00-02-u0yzh.mongodb.net:27017/test?ssl=true&replicaSet=firstResponse-shard-0&authSource=admin")
 db = client.firstResponse
@@ -9,3 +10,5 @@ resultResponders = db.firstResponders.delete_many({})
 print (resultResponders.deleted_count, "first responder documents have been deleted")
 resultVictims = db.victims.delete_many({})
 print (resultVictims.deleted_count, "victim documents have been delted")
+
+populateDummyData.addVictims()
--- populateDummyData.py
+++ populateDummyData.py
@@ -2,16 +2,23 @@ import dbUtils
 
 #addPerson( name, lat, lng, phone, isVictim )
 #Add Victims:
-dbUtils.addPerson("George", 39.951426, -75.179021, 1111111111, True)
-dbUtils.addPerson("Andrew", 39.947597, -75.180811, 1111111111, True)
-dbUtils.addPerson("Kevin", 39.943159, -75.176001, 1111111111, True)
-dbUtils.addPerson("John", 39.945502, -75.166682, 1111111111, True)
-dbUtils.addPerson("Jack", 39.970261, -75.178528, 1111111111, True)
-dbUtils.addPerson("Brandon", 39.935787, -75.220462, 1111111111, True)
-dbUtils.addPerson("Evan", 39.909964, -75.175217, 1111111111, True)
-dbUtils.addPerson("Tejas", 39.951850, -75.236655, 1111111111, True)
-dbUtils.addPerson("Muthukrishnan AKA Muthu", 39.950534, -75.202796, 1111111111, True)
-dbUtils.addPerson("Alexa", 39.938901, -75.182731, 1111111111, True)
+def addVictims():
+    dbUtils.addPerson("George", 39.951426, -75.179021, 1111111111, True)
+    dbUtils.addPerson("Andrew", 39.947597, -75.180811, 1111111111, True)
+    dbUtils.addPerson("Kevin", 39.943159, -75.176001, 1111111111, True)
+    dbUtils.addPerson("John", 39.945502, -75.166682, 1111111111, True)
+    dbUtils.addPerson("Jack", 39.970261, -75.178528, 1111111111, True)
+    dbUtils.addPerson("Brandon", 39.935787, -75.220462, 1111111111, True)
+    dbUtils.addPerson("Evan", 39.909964, -75.175217, 1111111111, True)
+    dbUtils.addPerson("Tejas", 39.951850, -75.236655, 1111111111, True)
+    dbUtils.addPerson("Muthukrishnan AKA Muthu", 39.950534, -75.202796, 1111111111, True)
+    dbUtils.addPerson("Alexa", 39.938901, -75.182731, 1111111111, True)
+    print("Added Victims!")
+
 
 #Add First Responders:
-dbUtils.addPerson("Harrison", 39.967382, -75.194042, 1111111111, False)
+def addFirstResponder():
+    dbUtils.addPerson("Harrison", 39.967382, -75.194042, 1111111111, False)
+    print("Added First Responders")
+
+
