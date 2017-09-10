7a4b273046d826f230627fe45a8d0b84dbbb7db3
U-DESKTOP-GIPVFTC\ranga <rangaraj.t@gmail.com>
2017-09-10 03:33:26 -0400

    changed Muthu's name

--- populateDummyData.py
+++ populateDummyData.py
@@ -9,9 +9,9 @@ def addVictims():
     dbUtils.addPerson("John", 39.945502, -75.166682, 1111111111, True)
     dbUtils.addPerson("Jack", 39.970261, -75.178528, 1111111111, True)
     dbUtils.addPerson("Brandon", 39.935787, -75.220462, 1111111111, True)
-    dbUtils.addPerson("Evan", 39.909964, -75.175217, 1111111111, True)
+    dbUtils.addPerson("Rigved", 39.909964, -75.175217, 1111111111, True)
     dbUtils.addPerson("Tejas", 39.951850, -75.236655, 1111111111, True)
-    dbUtils.addPerson("Muthukrishnan AKA Muthu", 39.950534, -75.202796, 1111111111, True)
+    dbUtils.addPerson("Muthukrishnamurthiswami Venkatamurthi AKA Muthu", 39.950534, -75.202796, 1111111111, True)
     dbUtils.addPerson("Alexa", 39.938901, -75.182731, 1111111111, True)
     print("Added Victims!")
 
